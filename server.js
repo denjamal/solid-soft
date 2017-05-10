b1dfdefb4bec965853ac93da248894cffe8ea532
denjamal <devmalimon@gmail.com>
2017-05-10 22:46:09 +0300

    Change port

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 app.use('/', express.static(__dirname)); // ← adjust
-app.listen(3000, function() { console.log('listening'); });
+app.listen(app.get('port'), function() { console.log('listening'); });
