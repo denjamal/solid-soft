e3e4243c1de7714bb1d4333e404b6fba1ed4ffc9
denjamal <devmalimon@gmail.com>
2017-07-08 22:12:09 +0300

    Images for slider were changed

--- js/main.js
+++ js/main.js
@@ -173,10 +173,9 @@ jQuery(function ($) {
 		animationDuration: 20000,
 		cover: true,
 		slides: [
-			{ src: 'https://ununsplash.imgix.net/reserve/RONyPwknRQOO3ag4xf3R_Kinsey.jpg?fit=crop&fm=jpg&h=700&q=75&w=1600' },
-			{ src: 'https://unsplash.imgix.net/photo-1414438992182-69e404046f80?fit=crop&fm=jpg&h=625&q=75&w=1600' },
-			{ src: 'https://unsplash.imgix.net/photo-1414490929659-9a12b7e31907?fit=crop&fm=jpg&h=800&q=75&w=1600' },
-			{ src: 'https://unsplash.imgix.net/uploads/14129863345840df499fc/0165574c?fit=crop&fm=jpg&h=600&q=75&w=1600' }
+			{ src: '../images/Slider/1.jpeg' },
+			{ src: '../images/Slider/2.jpeg' },
+			{ src: '../images/Slider/3.jpeg' },
 		]
 	});
 
