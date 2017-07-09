57f30226b63805fe71e12405fb6de7916ab40b02
denjamal <devmalimon@gmail.com>
2017-07-09 10:57:13 +0300

    Adjust email sending

--- js/main.js
+++ js/main.js
@@ -124,16 +124,20 @@ jQuery(function ($) {
 
 	// Contact form
 	var form = $('#main-contact-form');
-	form.submit(function (event) {
+	form.on("submit", function (event) {
 		event.preventDefault();
+		var data = form.serialize();	
 		var form_status = $('<div class="form_status"></div>');
 		$.ajax({
 			url: $(this).attr('action'),
 			beforeSend: function () {
 				form.prepend(form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn());
-			}
+			},
+			method: "POST",
+			data: data
 		}).done(function (data) {
 			form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
+			form.trigger("reset");
 		});
 	});
 
